376a4ef8f9b7fd4486784a4a714df1d4c1d97fc0
Minh Thanh <ngthachminhthanh@gmail.com>
2024-05-20 00:42:39 +0700

    update data

--- src/data.jsx
+++ src/data.jsx
@@ -3,67 +3,105 @@ import { v4 as uuidv4 } from "uuid";
 function chillHop() {
     return [
         {
-            name: "Beaver Creek",
+            name: "Kill Bill", 
             cover:
-                "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
-            artist: "Aso, Middle School, Aviino",
-            audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
-            color: ["#205950", "#2ab3bf"],
+                "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+            artist: "SZA",
+            audio: "https://github.com/q113pt/MusicLibrary/blob/main/SZA%20-%20Kill%20Bill.mp3?raw=true",
+            color: ["#CD607D", "#c94043"],
             id: uuidv4(),
             active: true,
         },
         {
-            name: "Daylight",
+            name: "BLUE",
             cover:
-                "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
-            artist: "Aiguille",
-            audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
-            color: ["#EF8EA9", "#ab417f"],
-            id: uuidv4(),
-            active: false,
-        },
-        {
-            name: "Keep Going",
-            cover:
-                "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
-            artist: "Swørn",
-            audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
-            color: ["#CD607D", "#c94043"],
-            id: uuidv4(),
-            active: false,
-        },
-        {
-            name: "Nightfall",
-            cover:
-                "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
-            artist: "Aiguille",
-            audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
-            color: ["#EF8EA9", "#ab417f"],
-            id: uuidv4(),
-            active: false,
-        },
-        {
-            name: "Reflection",
-            cover:
-                "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
-            artist: "Swørn",
-            audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
-            color: ["#CD607D", "#c94043"],
-            id: uuidv4(),
-            active: false,
-        },
-        {
-            name: "Under the City Stars",
-            cover:
-                "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
-            artist: "Aso, Middle School, Aviino",
-            audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
+                "https://images.genius.com/5e7bf410789d01a90983b2641b88e5bd.1000x1000x1.png",
+            artist: "Billie Eilish",
+            audio: "https://github.com/q113pt/MusicLibrary/blob/main/Billie%20Eilish%20-%20BLUE.mp3?raw=true",
             color: ["#205950", "#2ab3bf"],
             id: uuidv4(),
             active: false,
         },
-        //ADD MORE HERE
-        
+        {
+            name: "Blank Space",
+            cover:
+                "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+            artist: "Taylor Swift",
+            audio: "https://github.com/q113pt/MusicLibrary/blob/main/Taylor%20Swift%20-%20Blank%20Space.mp3?raw=true",
+            color: ["#EF8EA9", "#ab417f"],
+            id: uuidv4(),
+            active: false,
+        },
+        {
+            name: "Blinding Lights",
+            cover:
+                "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+            artist: "The Weekend",
+            audio: "https://github.com/q113pt/MusicLibrary/blob/main/The%20Weeknd%20-%20Blinding%20Lights.mp3?raw=true",
+            color: ["#CD607D", "#c94043"],
+            id: uuidv4(),
+            active: false,
+        },
+        {
+            name: "Ticking",
+            cover:
+                "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+            artist: "Zach Bryan",
+            audio: "https://github.com/q113pt/MusicLibrary/blob/main/Zach%20Bryan%20-%20Ticking.mp3?raw=true",
+            color: ["#205950", "#2ab3bf"],
+            id: uuidv4(),
+            active: false,
+        },
+        {
+            name: "CHIHIRO",
+            cover:
+                "https://i1.sndcdn.com/avatars-g5aRb8wklpfCUKIc-emVYkg-t500x500.jpg",
+            artist: "Billie Eilish",
+            audio: "https://github.com/q113pt/MusicLibrary/blob/main/Billie%20Eilish%20-%20CHIHIRO.mp3?raw=true",
+            color: ["#EF8EA9", "#ab417f"],
+            id: uuidv4(),
+            active: false,
+        },
+        {
+            name: "Love Story",
+            cover:
+                "https://i1.sndcdn.com/avatars-g5aRb8wklpfCUKIc-emVYkg-t500x500.jpg",
+            artist: "Taylor Swift",
+            audio: "https://github.com/q113pt/MusicLibrary/blob/main/Taylor%20Swift%20-%20Love%20Story.mp3?raw=true",
+            color: ["#EF8EA9", "#ab417f"],
+            id: uuidv4(),
+            active: false,
+        },
+        {
+            name: "Save Your Tears",
+            cover:
+                "https://i1.sndcdn.com/avatars-g5aRb8wklpfCUKIc-emVYkg-t500x500.jpg",
+            artist: "The Weekend",
+            audio: "https://github.com/q113pt/MusicLibrary/blob/main/The%20Weeknd%20-%20Save%20Your%20Tears.mp3?raw=true",
+            color: ["#EF8EA9", "#ab417f"],
+            id: uuidv4(),
+            active: false,
+        },
+        {
+            name: "SKINNY",
+            cover:
+                "https://i1.sndcdn.com/avatars-g5aRb8wklpfCUKIc-emVYkg-t500x500.jpg",
+            artist: "Billie Eilish",
+            audio: "https://github.com/q113pt/MusicLibrary/blob/main/Billie%20Eilish%20-%20SKINNY.mp3?raw=true",
+            color: ["#EF8EA9", "#ab417f"],
+            id: uuidv4(),
+            active: false,
+        },
+        {
+            name: "Peter",
+            cover:
+                "https://i1.sndcdn.com/avatars-g5aRb8wklpfCUKIc-emVYkg-t500x500.jpg",
+            artist: "Taylor Swift",
+            audio: "https://github.com/q113pt/MusicLibrary/blob/main/Taylor%20Swift%20-%20Peter.mp3?raw=true",
+            color: ["#EF8EA9", "#ab417f"],
+            id: uuidv4(),
+            active: false,
+        },
     ];
 }
 
